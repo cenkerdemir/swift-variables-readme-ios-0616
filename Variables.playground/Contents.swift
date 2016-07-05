1347788dad10ea85529c98247c8806c2267d3025
github username <github email address>
2016-07-05 12:23:52 -0400

    errors fixed

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -4,9 +4,10 @@ print(favoriteCharacter)
 favoriteCharacter = "Tyrion Lannister"
 print(favoriteCharacter)
 
-favoriteCharacter = 76
+favoriteCharacter = "Tyler Durden"
 
 let ultimateFavoriteCharacter = "Arya Stark"
 print(ultimateFavoriteCharacter)
 
-ultimateFavoriteCharacter = "Daenerys Targaryen"
+var newUltimateFavoriteCharacter = "Daenerys Targaryen"
+print(newUltimateFavoriteCharacter)
